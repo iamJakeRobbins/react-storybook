7444c408beec7c00f505ab4239d8abf94829a83b
Jake Robbins <jrobbinsd@gmail.com>
2017-12-04 08:22:02 -0700

    updated with first custom module/component

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,10 +5,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
+import Timer from '../components/Timer';
 import Message from '../components/Message';
 
 storiesOf('Message', module)
   .add('Hello World', () => <Message text="Hello world" />)
   .add("What's the time?", () => <Message text={new Date().toString()} />)
 	.add('custom', () => <Message text = 'Baby steps now!' />)
+
+	storiesOf('Timer', module)
+  .add('Show', () => <Timer />)
